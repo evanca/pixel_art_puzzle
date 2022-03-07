0e168b8e8e5ac0e8d8e34d21871041b63754666a
Ivanna Kaceviča <ivanna.kacevica@midis.eu>
2022-03-07 21:11:50 +0200

    Install Firebase Core for Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,20 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:pixel_art_puzzle/app/app.dart';
 import 'package:pixel_art_puzzle/bootstrap.dart';
 import 'package:pixel_art_puzzle/preferences/preferences.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs().init();
   SystemChrome.setPreferredOrientations(
           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
