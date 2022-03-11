4254aabb7eab86058ecc6c217a4d55a834b59d7c
Ivanna Kaceviča <ivanna.kacevica@midis.eu>
2022-03-11 20:48:01 +0200

    Fix Firebase options for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -15,15 +17,18 @@ import 'package:pixel_art_puzzle/preferences/preferences.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  FirebaseOptions options = const FirebaseOptions(
+  FirebaseOptions options = FirebaseOptions(
       apiKey: "AIzaSyCVoo-4HZGMmy8G4Ns2N4X_DjNEHqPJD6k",
       authDomain: "pixel-art-puzzle-a1e13.firebaseapp.com",
       databaseURL: "https://pixel-art-puzzle-a1e13-default-rtdb.firebaseio.com",
       projectId: "pixel-art-puzzle-a1e13",
       storageBucket: "pixel-art-puzzle-a1e13.appspot.com",
       messagingSenderId: "972766593370",
-      appId: "1:972766593370:web:820a4aa4d0ef32d49bf363",
-      measurementId: "G-6ZFRTLVNYS");
+      appId: Platform.isIOS
+          ? "1:972766593370:ios:919fde42a5678e0d9bf363"
+          : "1:972766593370:web:820a4aa4d0ef32d49bf363",
+      measurementId: "G-6ZFRTLVNYS",
+      iosBundleId: "studio.happycode.pixelArtPuzzle");
 
   await Firebase.initializeApp(
     options: options,
